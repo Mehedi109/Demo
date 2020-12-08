35ba898f51da206b251661ca96b61435c380a5d4
Mehedi109 <61519906+Mehedi109@users.noreply.github.com>
2020-12-08 17:55:01 +0600

    Update settings.py

--- demo_task/settings.py
+++ demo_task/settings.py
@@ -126,14 +126,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_URL = '/static/'
 if DEBUG:
     STATICFILES_DIRS=[
         os.path.join(BASE_DIR,'static'),
     ]
 else:
     STATIC_ROOT = os.path.join(BASE_DIR,'static')
-
+STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
